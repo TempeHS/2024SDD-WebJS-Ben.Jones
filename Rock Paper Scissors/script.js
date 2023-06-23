0c8da1e3996d8a78cf6fdd825d18eb14b9c922b0
Ben Jones <benpaddlejones@outlook.com>
2023-06-23 19:05:29 +1000

    Increase application security through encapsulation

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,27 +1,31 @@
-var randomNumber = 0;
-var playerScore = 0;
-var computerScore = 0;
-var playerTip = 0;
+let playerScore;
+let computerScore;
 
+function gameLoop (playerClicked) {
+	const randomNumber = generateRandomNumber()
+	// convert random number to a choice so you can both equality check and use in the ouput string it should be stored in a const 
+	
 
-function gameLoop (playerGuess) {
-	equalityCheck (playerGuess, generateRandomNumber());
-	updateHtmlContent ();
+	
+	equalityCheck (playerClicked, computerChoice);
 }
 
 function generateRandomNumber () {
-	var result = Math.floor((Math.random() * 3) + 1);
+	const result = Math.floor((Math.random() * 3) + 1);
 	return result;
 }
 
-function updateHtmlContent () {
+
+function equalityCheck (playerChoice, computerChoice){
+	// perform an equality check and update scores and out put a string using concatenation that gives user feedback 
+
+
+
+	alert (playerChoice);  //for debug only
+	alert (computerChoice); //for debug only
+
+	const playerTip = 0;
 	document.getElementById("playerScoreContent").innerHTML = playerScore;
 	document.getElementById("computerScoreContent").innerHTML = computerScore;
-	document.getElementById("tipContent").innerHTML = playerTip;	
-}
-
-
-function equalityCheck (playerGuess, generateRandomNumber){
-	alert (playerGuess);
-	alert (generateRandomNumber);
+	document.getElementById("tipContent").innerHTML = playerTip;
 }
